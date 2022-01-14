dc9a39aeacd65425f444e8e1335c5a1da9a7fdd9
penyaa <91029232+penyaa@users.noreply.github.com>
2022-01-14 20:22:46 +0300

    Add files via upload

---
+++ kursovaya/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <!-- <div class="container-fluid">
+        <nav class="fixed-top">
+            <div class="row">
+                <div class="col-lg-2 text-right">
+                    <a class="navbar-brand" href="#">
+                        <img src="pic/logo.png" class="w-25" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-10 text-center">
+                    <div class="nav mt-3">
+                        <a class="nav-link" href="#">Главная</a>
+                        <a class="nav-link" href="#">Поиск станций велопроката</a>
+                        <a class="nav-link" href="#">Все станции проката Москвы</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div> -->
+    <div class="container-fluid">
+        <nav class="nav">
+            <div class="row">
+                <div class="col-lg-3 text-right mt-1">
+                    <a class="navbar-brand" href="#">
+                        <img width="100" height="100" src="pic/logo.png" class="w-20 img-fluid" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 text-start">
+                    <a href="index.php" class="nav-link fs-4 mt-4">Главная</a>
+                </div>   
+                <div class="col-lg-3 text-start">
+                    <a href="search.php" class="nav-link fs-4 mt-4">Поиск станций велопроката</a>
+                </div>
+                <div class="col-lg-3 text-start">
+                    <a href="allstations.php" class="nav-link fs-4 mt-4">Все станции проката Москвы</a>
+                </div>
+            </div>
+        </nav>
